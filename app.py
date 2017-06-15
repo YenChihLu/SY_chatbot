5937f2475539683fe3fdcb310bd58125789eaaf3
Finley Huang <finley.huang@waystorm.com>
2017-06-15 11:43:34 +0800

    67

--- app.py
+++ app.py
@@ -49,8 +49,9 @@ def webhook():
                             req = ai.text_request()
                             req.lang = 'zh-TW'
                             req.query=messaging_event["message"]["text"].encode('UTF8')
-                            response = req.getresponse()
-                            print response
+                            response = req.getresponse().json()
+                            print '------',response
+                            print response.read()
                             answers = response["result"]["fulfillment"]["messages"][0]["speech"]
                             send_message(sender_id, answers)
 
