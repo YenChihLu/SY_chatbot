6bd5ec076f518cce5e2bc99c548539dca725f725
Finley Huang <finley.huang@waystorm.com>
2017-06-15 13:06:14 +0800

    67

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def webhook():
                             response = req.getresponse()
                             print '------',response
                             print response.read(),type(response.read())
-                            response = json.loads(response.read())
+                            response = json.loads(response.read().decode('utf8'))
                             answers = response["result"]["fulfillment"]["messages"][0]["speech"]
                             send_message(sender_id, answers)
 
